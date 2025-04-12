f529d31989a2cbbecdc6df3e6276de8627b13a25
takumi <takumidiary.0927@gmail.com>
2025-04-12 17:34:12 +0900

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
-  integrations: [icon()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	site: "https://takumidiary.com",
+	integrations: [icon(), sitemap()],
 });
